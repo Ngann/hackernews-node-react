16042c65a43f7fbe49cde34ddcdeed394f079eea
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-02 15:24:47 -0800

    deployed AWS aurora using prisma

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ in root director run `node src/index.js` to access  GraphQL Playground in http:/
 schema.graphql file defines the schema which is defined in typeDefs in the index.js
 
 mutation is then added to the resolver which pushes to the links array.
+`prisma deploy` to deploy server and connect to AWS aurora
+
+cd prisma, run `prisma generate`  now generated and located in hackernews-node/src/generated/prisma-client
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
 // links variable is used to store the links at runtime.
 let links = [{
@@ -10,22 +11,30 @@ let links = [{
 let idCount = links.length
 // add new resolver for the feed root field. Notice that a resolver always has to be named after the corresponding field from the schema definition(type Query).
 // Finally, you’re adding three more resolvers for the fields on the Link type from the schema definition. We’ll discuss in a bit what the parent argument is that’s passed into the resolver here.
+
+
+//all GraphQL resolver functions always receive four arguments.
+
+//the feed resolver It accesses a prisma object on context. As you will see in a bit, this prisma object actually is a Prisma client instance that’s imported from the generated prisma-client library.
+
+// This Prisma client instance effectively lets you access your database through the Prisma API. It exposes a number of methods that let you perform CRUD operations for your models.
+
+
+//As arguments, you’re passing the data that the resolvers receive via the args parameter.
 const resolvers = {
   Query: {
     info: () => `this is the API of hackernews Clone`,
-    feed: () => links,
+    feed: () => (root, args, context, info) =>{
+      return context.prisma.links()
+    },
   },
-
   Mutation: {
-    post: (parent, args) => {
-      const link ={
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-      links.push(link)
-      return link
-    }
+        description: args.description,
+      })
+    },
   },
 
   Link: {
@@ -38,6 +47,7 @@ const resolvers = {
 const server = new GraphQLServer ( {
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {prisma},
 })
 
 server.start(() => console.log(` Server is running on http://localhost:4000`))
