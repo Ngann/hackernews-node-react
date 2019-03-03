9b8394c10e647f40986975e3ba785198825f0d9c
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-02 18:10:22 -0800

    add resolver files to move rsolver from index

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
+## Usage
+
+### 1. Clone repository & install dependencies
+
+```sh
+git clone 
+cd graphql-js
+yarn install # or `npm install`
+```
+
+### 2. Install the Prisma CLI
+
+```sh
+yarn global add prisma
+```
+
+You need to setup a Prisma service. You can refer to [this Quickstart](https://www.prisma.io/docs/quickstart/) to learn how.
+
+### 3. Deploy the Prisma database service
+
+```sh
+prisma deploy
+```
+
+When prompted where (i.e. to which Prisma server) you want to deploy your service, choose the **Demo server** which can be used for free in Prisma Cloud. If you haven't done so already, you will be asked to register with Prisma Cloud (which you can do via GitHub). For the following prompts in the terminal you can select the suggested values by hitting Enter. (If you have Docker installed, you can also choose to deploy Prisma locally by creating a new database.)
+
+### 4. Start the server & open Playground
+
+To interact with the API in a GraphQL Playground, all you need to do is execute the `start` script defined in `package.json`:
+
+```sh
+yarn start
+```
+
+
+
 in root director run `node src/index.js` to access  GraphQL Playground in http://localhost:4000
 
 schema.graphql file defines the schema which is defined in typeDefs in the index.js
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,36 +1,35 @@
-// The signup mutation, password = encrypts the User’s password using the bcryptjs. ( need to install bcrypt)
-// user = use the prisma client instance to store the new User in the database.
-// token = generate JWT which is signed with an APP_SECRET. (need to install jws library and create APP_SECRET)
-// return  the token and user in a object that adheres to the shape of of an AuthPayload object from GraphQL schema.
-
-
 async function signup(parent, args, context, info) {
+  // 1
   const password = await bcrypt.hash(args.password, 10)
-  const user = await context.prisma.createUser({...args, password})
-  const token = jwt.sign({userId: user.id}, APP_SECRET)
+  // 2
+  const user = await context.prisma.createUser({ ...args, password })
+
+  // 3
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  // 4
   return {
     token,
     user,
   }
 }
 
-
-//user = Instead of creating a new User object, we are using the prisma client instance to retrieve the existing User record by the email address that was sent along as an argument in the login mutation. If no User with that email address was found, this will return a corresponding error.
-//then compare the provided password with the one that is stored in the database. If the two don’t match, you’re returning an error as well.
-
-async function login(parent, args, contect, info) {
-  const user = await context.prisma.user({email: args.email})
-  if (!user ) {
+async function login(parent, args, context, info) {
+  // 1
+  const user = await context.prisma.user({ email: args.email })
+  if (!user) {
     throw new Error('No such user found')
   }
 
-  const valid = await bcrypt.,compare(args.password, user.password)
-  if(!valid) {
+  // 2
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) {
     throw new Error('Invalid password')
   }
 
-  const token = jwt.sign({userId: user.id}, APP_SECRET)
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
 
+  // 3
   return {
     token,
     user,
