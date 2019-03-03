f44f51a186de4970c1f7241230f9322ceb713f8e
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-03-02 18:40:03 -0800

    token authentication works, error for name = email reference unique user must be added, check the database to make sure there's no duplicate

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -12,15 +12,11 @@ function post(parent, args, context, info) {
 }
 
 async function signup(parent, args, context, info) {
-  // 1
   const password = await bcrypt.hash(args.password, 10)
-  // 2
   const user = await context.prisma.createUser({ ...args, password })
 
-  // 3
   const token = jwt.sign({ userId: user.id }, APP_SECRET)
 
-  // 4
   return {
     token,
     user,
@@ -28,13 +24,11 @@ async function signup(parent, args, context, info) {
 }
 
 async function login(parent, args, context, info) {
-  // 1
   const user = await context.prisma.user({ email: args.email })
   if (!user) {
     throw new Error('No such user found')
   }
 
-  // 2
   const valid = await bcrypt.compare(args.password, user.password)
   if (!valid) {
     throw new Error('Invalid password')
@@ -42,15 +36,15 @@ async function login(parent, args, context, info) {
 
   const token = jwt.sign({ userId: user.id }, APP_SECRET)
 
-  // 3
   return {
     token,
     user,
   }
 }
 
+
 module.exports = {
+  post,
   signup,
   login,
-  post,
 }
